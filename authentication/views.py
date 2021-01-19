cab958e61fb63224d2b952d4afe006c5f69a8785
zestrixx <mayank01.kv@gmail.com>
2021-01-19 19:50:04 +0530

    Initial commit

---
+++ authentication/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
+
+
+def login(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            # handle login
+            if request.POST['email'] and request.POST['password']:
+                try:
+                    user = User.objects.get(email=request.POST['email'])
+                    auth.login(request, user)
+
+                    if request.POST['next'] != '':
+                        return redirect(request.POST.get('next'))
+                    else:
+                        return redirect('/')
+                except User.DoesNotExist:
+                    return render(request, 'login.html', {'error': "User Doesn't Exist"})
+            else:
+                return render(request, 'login.html', {'error': "Empty Fields"})
+        else:
+            return render(request, 'login.html')
+    else:
+        return redirect('/')
+
+
+def signup(request):
+    if request.method == "POST":
+        # handle signup
+        if request.POST['password'] == request.POST['password2']:
+            if request.POST['username'] and request.POST['email'] and request.POST['password']:
+                try:
+                    user = User.objects.get(email=request.POST['email'])
+                    return render(request, 'signup.html', {'error': "User Already Exists"})
+                except User.DoesNotExist:
+                    User.objects.create_user(
+                        username=request.POST['username'],
+                        email=request.POST['email'],
+                        password=request.POST['password'],
+                    )
+                    messages.success(
+                        request, "Signup Successful! Login To continue")
+                    return redirect(login)
+            else:
+                return render(request, 'signup.html', {'error': "Empty Fields"})
+        else:
+            return render(request, 'signup.html', {'error': "Password's Don't Match"})
+    else:
+        return render(request, 'signup.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/login')
