977c540aeb0953b44bd01fa14c10e80dcece4c0c
zestrixx <mayank01.kv@gmail.com>
2021-06-09 02:14:46 +0530

    modified name of the heroku app

--- URLshortener/settings.py
+++ URLshortener/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["urlshortener-shortly.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["yourlshortener.herokuapp.com", "localhost"]
 
 
 # Application definition
